1b8f86cca6b1ae614b06e40e224ac6d71500e106
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-05-20 12:40:17 -0700

    importa funciones de comunicacion serial

---
+++ MonitoringSystem/Core/Services/ISerialCommunication.cs
@@ -0,0 +1,10 @@
+﻿namespace MonitoringSystem.Core.Services
+{
+    internal interface ISerialCommunication
+    {
+        void Open();
+        void Close();
+        void WriteLine(string message);
+        string ReadLine();
+    }
+}
---
+++ MonitoringSystem/Infrastructure/SerialComm/SerialCommunication.cs
@@ -0,0 +1,49 @@
+﻿using System.IO.Ports;
+
+namespace MonitoringSystem.Infrastructure.SerialComm
+{
+    internal class SerialCommunication : Core.Services.ISerialCommunication
+    {
+        private readonly SerialPort _serialPort;
+        public SerialCommunication(string portName, int baudRate)
+        {
+            _serialPort = new SerialPort(portName, baudRate)
+            {
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                Handshake = Handshake.None,
+                ReadTimeout = 500,
+                WriteTimeout = 500
+            };
+        }
+        public void Open()
+        {
+            if (!_serialPort.IsOpen)
+            {
+                _serialPort.Open();
+                Console.WriteLine($"Conexion establecida con {_serialPort.PortName}");
+            }
+        }
+        public void Close()
+        {
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.Close();
+                Console.WriteLine("Conexion cerrada");
+            }
+        }
+        public string ReadLine()
+        {
+            Thread.Sleep(5000);
+            if (_serialPort.BytesToRead > 0)
+                return $"Respuesta recibida: {_serialPort.ReadExisting()}";
+            else
+                return "No se recibio respuesta.";
+        }
+        public void WriteLine(string message)
+        {
+            _serialPort.WriteLine($"\x01{message}");
+        }
+    }
+}
