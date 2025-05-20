eabb086c51baac761a86dbc6bd22135c043bca1f
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-05-20 12:40:54 -0700

    interaccion con interfaz con la comunicacion

--- MonitoringSystem/Program.cs
+++ MonitoringSystem/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MonitoringSystem.Infrastructure.SerialComm;
+
+var serialComm = new SerialCommunication("COM3", 2400);
+
+serialComm.Open();
+while (true)
+{
+    Console.Write(">> ");
+    string command = Console.ReadLine() ?? "";
+    
+    if (command.ToLower() == "exit") break;
+
+    serialComm.WriteLine(command);
+    string response = serialComm.ReadLine();
+    Console.WriteLine(response);
+}
+
+serialComm.Close();
