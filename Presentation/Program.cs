61ae4cf6ca2adad43763dbaec39bf56b07df859c
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-05-21 15:31:38 -0700

    Configuraciones default acceso base datos

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Application.Services;
+﻿using Application.Services;
 using Domain.Interfaces;
 using Infrastructure.Communication;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
+// Configuración
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+// DI
 var services = new ServiceCollection();
 
-// Configuración de DI
-services.AddTransient<ISerialCommunication>(_ =>
-    new SerialCommunication("COM3", 2400));
-services.AddTransient<SerialService>();
+// Configuración de la base de datos
+services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+// Configuración del servicio serial
 
 var provider = services.BuildServiceProvider();
 
