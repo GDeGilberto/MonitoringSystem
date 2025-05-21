3ec5b988e9be8a6c89be2194626cc1f4b7a82bbc
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-05-21 10:23:03 -0700

    creacion capa Domain

---
+++ Domain/Interfaces/ISerialCommunication.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Interfaces
+{
+    public interface ISerialCommunication
+    {
+        void Open();
+        void Close();
+        string Read();
+        void Write(string message);
+        bool IsOpen { get; }
+    }
+}
