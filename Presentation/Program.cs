7087f7c205236baa6ab9e2fd920958d540aff608
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-05-21 10:25:24 -0700

    creacion modulo presentation con referencia a application e infrastructure

---
+++ Presentation/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Communication;
+
+var services = new ServiceCollection();
+
+// Configuración de DI
+services.AddTransient<ISerialCommunication>(_ =>
+    new SerialCommunication("COM3", 2400));
+services.AddTransient<SerialService>();
+
+var provider = services.BuildServiceProvider();
+
+// Ejecución
+var serialService = provider.GetRequiredService<SerialService>();
+await serialService.RunAsync();
