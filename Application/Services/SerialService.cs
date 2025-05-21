8fbf57f0a3c459edfe7befaf34bcfe34fc548db2
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-05-21 10:24:17 -0700

    creacion modulo application con referencia a domain

---
+++ Application/Services/SerialService.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class SerialService
+    {
+        private readonly ISerialCommunication _serialComm;
+
+        public SerialService(ISerialCommunication serialComm)
+        {
+            _serialComm = serialComm;
+        }
+
+        public async Task RunAsync()
+        {
+            _serialComm.Open();
+
+            try
+            {
+                while (true)
+                {
+                    Console.Write(">> ");
+                    string command = Console.ReadLine() ?? "";
+
+                    if (command.ToLower() == "exit") break;
+
+                    _serialComm.Write(command);
+                    string response = _serialComm.Read();
+                    Console.WriteLine(response);
+                }
+            }
+            finally
+            {
+                _serialComm.Close();
+            }
+        }
+    }
+}
