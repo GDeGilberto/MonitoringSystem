577cfbe8af9fc19ab4f0c9cc6d60df254bcad0ca
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-04 13:44:58 -0700

    Creacion bases para crud con descargas

---
+++ Domain/Entities/DescargasEntity.cs
@@ -0,0 +1,48 @@
+﻿namespace Domain.Entities
+{
+    public class DescargasEntity
+    {
+        public int Id { get; }
+        public int IdEstacion { get; }
+        public int NoTanque { get; }
+        public double VolumenInicial { get; }
+        public double? TemperaturaInicial { get; }
+        public DateTime FechaInicial { get; }
+        public double VolumenDisponible { get; }
+        public double? TemperaturaFinal { get; }
+        public DateTime FechaFinal { get; }
+        public double CantidadCargada { get; }
+
+        public DescargasEntity(int idEstacion, int noTanque, double volumenInicial, 
+            double? temperaturaInicial, DateTime fechaInicial, double volumenDisponible, 
+            double? temperaturaFinal, DateTime fechaFinal, double cantidadCargada)
+        {
+            IdEstacion = idEstacion;
+            NoTanque = noTanque;
+            VolumenInicial = volumenInicial;
+            TemperaturaInicial = temperaturaInicial;
+            FechaInicial = fechaInicial;
+            VolumenDisponible = volumenDisponible;
+            TemperaturaFinal = temperaturaFinal;
+            FechaFinal = fechaFinal;
+            CantidadCargada = cantidadCargada;
+        }
+
+        public DescargasEntity(int id ,int idEstacion, int noTanque, double volumenInicial, 
+            double? temperaturaInicial, DateTime fechaInicial, double volumenDisponible, 
+            double? temperaturaFinal, DateTime fechaFinal, double cantidadCargada)
+        {
+            Id = id;
+            IdEstacion = idEstacion;
+            NoTanque = noTanque;
+            VolumenInicial = volumenInicial;
+            TemperaturaInicial = temperaturaInicial;
+            FechaInicial = fechaInicial;
+            VolumenDisponible = volumenDisponible;
+            TemperaturaFinal = temperaturaFinal;
+            FechaFinal = fechaFinal;
+            CantidadCargada = cantidadCargada;
+        }
+    }
+
+}
