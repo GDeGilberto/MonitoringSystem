7adab9be57a64d965704cda54b7ed7f9c06e5b6e
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-09 15:59:46 -0700

    Crea skeleto de productos

--- Web/Components/Productos/ProductosComponent.razor.cs
+++ Web/Components/Productos/ProductosComponent.razor.cs
@@ -8,6 +8,9 @@ namespace Web.Components.Productos
         [Parameter]
         public List<ProductoViewModel> Productos { get; set; }
 
+        [Parameter]
+        public bool IsLoading { get; set; }
+
         private string FormatearDecimal(decimal? valor)
             => valor.HasValue ? valor.Value.ToString("N2") : "-";
     }
