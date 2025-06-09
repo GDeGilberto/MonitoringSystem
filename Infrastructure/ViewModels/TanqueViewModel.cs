f25daa24de29096ae66efe83c0e2fbac023cb4e4
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-09 09:03:52 -0700

    Creacion componente tabla de Tanques

---
+++ Infrastructure/ViewModels/TanqueViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Infrastructure.ViewModels
+{
+    public class TanqueViewModel
+    {
+        public int NoTanque { get; set; }
+        public TipoProducto Producto { get; set; }
+        public decimal? Volumen { get; set; }
+        public decimal? Capacidad { get; set; }
+        public decimal? Temperatura { get; set; }
+    }
+}
---
+++ Web/Components/Tanques/TanquesComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Infrastructure.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace Web.Components.Tanques
+{
+    public partial class TanquesComponent : ComponentBase
+    {
+        [Parameter]
+        public EstacionesEntity Estacion { get; set; }
+
+        [Parameter]
+        public List<TanqueViewModel>? Tanques { get; set; }
+
+        private string FormatearDecimal(decimal? valor)
+            => valor.HasValue ? valor.Value.ToString("N2") : "-";
+    }
+}
