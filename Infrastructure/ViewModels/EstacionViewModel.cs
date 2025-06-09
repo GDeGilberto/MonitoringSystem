dbbf9c13b3218185f8aa6b9eafcca24b5476e702
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-09 09:01:09 -0700

    Creacion componente estacionCard

---
+++ Infrastructure/ViewModels/EstacionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.ViewModels
+{
+    public class EstacionViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Web/Components/Estacion/EstacionComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace Web.Components.Estacion
+{
+    public partial class EstacionComponent : ComponentBase
+    {
+        [Parameter]
+        public EstacionViewModel Estacion { get; set; }
+    }
+}
