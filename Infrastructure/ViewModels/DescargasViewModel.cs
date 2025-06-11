fd4714802f4f283b82f3da32bce9c1f15f916ab9
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-11 16:45:37 -0700

    Creacion viewmodel de descargas

---
+++ Infrastructure/ViewModels/DescargasViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.ViewModels
+{
+    public class DescargasViewModel
+    {
+        public int NoTanque { get; set; }
+        public string VolumenInicial { get; set; }
+        public string? TemperaturaInicial { get; set; }
+        public string FechaInicial { get; set; }
+        public string VolumenDisponible { get; set; }
+        public string? TemperaturaFinal { get; set; }
+        public string FechaFinal { get; set; }
+        public string CantidadCargada { get; set; }
+    }
+}
