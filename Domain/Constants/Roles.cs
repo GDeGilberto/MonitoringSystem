64e12ae14b8f91b36ac96caef9f35794543ab4c6
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-16 11:17:07 -0700

    Agrega opcion agregar rol a crear cuenta

---
+++ Domain/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "Administrador";
+        public const string User = "Usuario";
+
+        public static readonly List<string> AllRoles = new List<string> { User, Admin };
+    }
+}
